1ed99e4e27f56ef00de7a9a5cd53b5bc44d7dbb4
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-07-14 17:08:29 +0200

    add required packages

--- setup.py
+++ setup.py
@@ -40,7 +40,10 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
-    install_requires=[],  # FIXME: add your package's dependencies to this list
+    install_requires=[
+        'numpy',
+        'dask[complete]',
+    ],
     setup_requires=[
         # dependency for `python setup.py test`
         'pytest-runner',
