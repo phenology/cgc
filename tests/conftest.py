c290441b116210a342a59f55dcce8abf26bb507d
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-07-22 00:27:42 +0200

    clean up dask worker space after use

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import shutil
 import pytest
 
 from dask.distributed import Client, LocalCluster
@@ -5,9 +6,12 @@ from dask.distributed import Client, LocalCluster
 
 @pytest.fixture(scope="session")
 def dask_client():
-    cluster = LocalCluster(n_workers=1, threads_per_worker=2)
+    dask_local_path = 'dask-worker-space'
+    cluster = LocalCluster(n_workers=1, threads_per_worker=2,
+                           local_directory=dask_local_path)
     client = Client(cluster)
     yield client
     # teardown
     client.close()
     cluster.close()
+    shutil.rmtree(dask_local_path)
