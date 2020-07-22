9edb8ff8d02e51ad1ffa579cfa4365ed9de36c4c
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-07-22 17:49:37 +0200

    add triclustering tests

---
+++ tests/test_triclustering.py
@@ -0,0 +1,27 @@
+import pytest
+
+import numpy as np
+import dask.array as da
+
+from geoclustering.triclustering import Triclustering
+
+
+@pytest.fixture
+def triclustering():
+    np.random.seed(1234)
+    da.random.seed(1234)
+    d, m, n = 3, 4, 3
+    ncl_row, ncl_col, ncl_bnd = 2, 2, 2
+    Z = np.random.randint(100, size=(d, m, n)).astype('float64')
+    return Triclustering(Z, nclusters_row=ncl_row, nclusters_col=ncl_col,
+                         nclusters_bnd=ncl_bnd, conv_threshold=1.e-5,
+                         max_iterations=100, nruns=10, epsilon=1.e-8)
+
+
+class TestTriclustering:
+    def test_run_with_threads(self, triclustering):
+        triclustering.run_with_threads(nthreads=2)
+        assert isinstance(triclustering.row_clusters, np.ndarray)
+        assert isinstance(triclustering.col_clusters, np.ndarray)
+        assert isinstance(triclustering.bnd_clusters, np.ndarray)
+        assert np.isclose(triclustering.error, -4638.9183068944785)
