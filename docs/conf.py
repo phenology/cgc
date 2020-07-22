74e09a46caee02c7151b02609cf7bb3e57995c74
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-07-22 21:56:12 +0200

    move import statement in conf.py
    package is not installed, should be imported after adding '..' to sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,10 @@
 import os
 import sys
 
-import geoclustering
-
 here = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
+
+import geoclustering
 
 
 # -- General configuration ------------------------------------------------
