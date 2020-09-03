82d5750cb6ecc0f2b2c19222b744ef327b28ba33
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-09-03 13:11:33 +0200

    add numpy encoder to serialize results even with np.ndarray

--- cgc/results.py
+++ cgc/results.py
@@ -1,7 +1,16 @@
 import datetime
 import json
 
+import numpy as np
+
 from . import __version__
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 
 class Results(object):
@@ -29,7 +38,7 @@ class Results(object):
         """
         if filename:
             with open(filename, 'w') as f:
-                json.dump(self.__dict__, f, indent=4)
+                json.dump(self.__dict__, f, indent=4, cls=NumpyEncoder)
 
     def __setattr__(self, name, value):
         self.__dict__['time_updated'] = datetime.datetime.now().isoformat()
