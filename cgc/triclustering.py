eb6a8df9990909dca35a7d7a8cbdf1536d904f40
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-09-04 15:25:21 +0200

    update triclustering with results object (with tests)

--- cgc/triclustering.py
+++ cgc/triclustering.py
@@ -1,13 +1,26 @@
 import concurrent.futures
-import json
 import logging
 
 from concurrent.futures import ThreadPoolExecutor
 
-from . import __version__
 from . import triclustering_numpy
 
+from .results import Results
+
 logger = logging.getLogger(__name__)
+
+
+class TriclusteringResults(Results):
+    """
+    Contains results and metadata of a tri-clustering calculation
+    """
+    def reset(self):
+        self.row_clusters = None
+        self.col_clusters = None
+        self.bnd_clusters = None
+        self.error = None
+        self.nruns_completed = 0
+        self.nruns_converged = 0
 
 
 class Triclustering(object):
@@ -16,7 +29,8 @@ class Triclustering(object):
     """
     def __init__(self, Z, nclusters_row, nclusters_col, nclusters_bnd,
                  conv_threshold=1.e-5, max_iterations=1, nruns=1,
-                 epsilon=1.e-8, output_filename=''):
+                 epsilon=1.e-8, output_filename='', row_clusters_init=None,
+                 col_clusters_init=None, bnd_clusters_init=None):
         """
         Initialize the object
 
@@ -29,8 +43,11 @@ class Triclustering(object):
         :param nruns: number of differntly-initialized runs
         :param epsilon: numerical parameter, avoids zero arguments in log
         :param output_filename: name of the file where to write the clusters
+        :param row_clusters_init: initial row clusters
+        :param col_clusters_init: initial column clusters
+        :param bnd_clusters_init: initial band clusters
         """
-        self.Z = Z
+        # Input parameters -----------------
         self.nclusters_row = nclusters_row
         self.nclusters_col = nclusters_col
         self.nclusters_bnd = nclusters_bnd
@@ -39,15 +56,24 @@ class Triclustering(object):
         self.nruns = nruns
         self.epsilon = epsilon
         self.output_filename = output_filename
+        self.row_clusters_init = row_clusters_init
+        self.col_clusters_init = col_clusters_init
+        self.bnd_clusters_init = bnd_clusters_init
+        # Input parameters end -------------
 
-        self.client = None
+        # store input parameters in results object
+        self.results = TriclusteringResults(**self.__dict__)
 
-        self.row_clusters = None
-        self.col_clusters = None
-        self.bnd_clusters = None
-        self.error = None
+        assert Z.ndim == 3, 'Incorrect dimensionality for Z matrix'
+        self.Z = Z
 
-        self.nruns_completed = 0
+        if all([cls is not None for cls in [row_clusters_init,
+                                            col_clusters_init,
+                                            bnd_clusters_init]]):
+            assert nruns == 1, 'Only nruns = 1 for given initial clusters'
+            assert Z.shape == (len(bnd_clusters_init),
+                               len(row_clusters_init),
+                               len(col_clusters_init))
 
     def run_with_threads(self, nthreads=1):
         """
@@ -55,6 +81,7 @@ class Triclustering(object):
         (only suitable for local runs)
 
         :param nthreads: number of threads
+        :return: tri-clustering results
         """
         with ThreadPoolExecutor(max_workers=nthreads) as executor:
             futures = {
@@ -66,60 +93,31 @@ class Triclustering(object):
                                 self.conv_threshold,
                                 self.max_iterations,
                                 self.epsilon,
-                                row_clusters_init=self.row_clusters,
-                                col_clusters_init=self.col_clusters,
-                                bnd_clusters_init=self.bnd_clusters
+                                row_clusters_init=self.row_clusters_init,
+                                col_clusters_init=self.col_clusters_init,
+                                bnd_clusters_init=self.bnd_clusters_init
                                 ):
                 r for r in range(self.nruns)
             }
             for future in concurrent.futures.as_completed(futures):
-                logger.info(f'Waiting for run {self.nruns_completed} ..')
+                logger.info(f'Waiting for run {self.results.nruns_completed}')
                 converged, niters, row, col, bnd, e = future.result()
                 logger.info(f'Error = {e}')
                 if converged:
                     logger.info(f'Run converged in {niters} iterations')
                 else:
                     logger.warning(f'Run not converged in {niters} iterations')
-                if self.error is None or e < self.error:
-                    self.row_clusters = row
-                    self.col_clusters = col
-                    self.bnd_clusters = bnd
-                    self.error = e
-                self.nruns_completed += 1
-        self._write_clusters()
+                if self.results.error is None or e < self.results.error:
+                    self.results.row_clusters = row
+                    self.results.col_clusters = col
+                    self.results.bnd_clusters = bnd
+                    self.results.error = e
+                self.results.nruns_completed += 1
+        self.results.write(filename=self.output_filename)
+        return self.results
 
     def run_with_dask(self, client=None):
         raise NotImplementedError
 
     def run_serial(self):
         raise NotImplementedError
-
-    def set_initial_clusters(self, row_clusters, col_clusters, bnd_clusters):
-        """
-        Set initial cluster assignment
-
-        :param row_clusters: initial row clusters
-        :param col_clusters: initial column clusters
-        :param bnd_clusters: initial band clusters
-        """
-        if (not (row_clusters is None
-                 and col_clusters is None
-                 and bnd_clusters is None)
-                and self.nruns > 1):
-            logging.warning('Multiple runs with the same cluster '
-                            'initialization will be performed.')
-        self.row_clusters = row_clusters
-        self.col_clusters = col_clusters
-        self.bnd_clusters = bnd_clusters
-
-    def _write_clusters(self):
-        if self.output_filename:
-            with open(self.output_filename, 'w') as f:
-                data = {
-                    'cgc_version': __version__,
-                    'error': self.error,
-                    'row_clusters': self.row_clusters.tolist(),
-                    'col_clusters': self.col_clusters.tolist(),
-                    'bnd_clusters': self.bnd_clusters.tolist()
-                }
-                json.dump(data, f, indent=4)
