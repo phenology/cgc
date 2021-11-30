7d73bcc281544831ef313e09f90af925c5af215f
Francesco Nattino <f.nattino@esciencecenter.nl>
2021-11-30 23:59:42 +0100

    fix triclustering

--- cgc/triclustering_dask.py
+++ cgc/triclustering_dask.py
@@ -9,9 +9,8 @@ from dask.distributed import get_client
 logger = logging.getLogger(__name__)
 
 
-def _distance(Z, Y, epsilon):
+def _distance(Z, Y):
     """ Distance function """
-    Y = Y + epsilon
     return Y.sum(axis=(1, 2)) - da.einsum('ijk,ljk->il', Z, da.log(Y))
 
 
@@ -21,14 +20,14 @@ def _initialize_clusters(n_el, n_clusters, chunks=None):
     return da.random.permutation(cluster_idx)
 
 
-def _setup_cluster_matrix(n_clusters, cluster_idx):
+def _setup_cluster_matrix(cluster_labels, cluster_idx):
     """ Set cluster occupation matrix """
-    return da.eye(n_clusters, dtype=np.bool, chunks=n_clusters)[cluster_idx]
+    return da.equal.outer(cluster_idx, cluster_labels)
 
 
 def triclustering(Z, nclusters_row, nclusters_col, nclusters_bnd, errobj,
-                  niters, epsilon, row_clusters_init=None,
-                  col_clusters_init=None, bnd_clusters_init=None):
+                  niters, row_clusters_init=None, col_clusters_init=None,
+                  bnd_clusters_init=None):
     """
     Run the tri-clustering analysis, Dask implementation.
 
@@ -45,9 +44,6 @@ def triclustering(Z, nclusters_row, nclusters_col, nclusters_bnd, errobj,
     :type errobj: float, optional
     :param niters: Maximum number of iterations.
     :type niters: int, optional
-    :param epsilon: Numerical parameter, avoids zero arguments in the
-        logarithm that appears in the expression of the objective function.
-    :type epsilon: float, optional
     :param row_clusters_init: Initial row cluster assignment.
     :type row_clusters_init: numpy.ndarray or array_like, optional
     :param col_clusters_init: Initial column cluster assignment.
@@ -87,34 +83,45 @@ def triclustering(Z, nclusters_row, nclusters_col, nclusters_bnd, errobj,
         nel_row_clusters = da.bincount(row_clusters, minlength=nclusters_row)
         nel_col_clusters = da.bincount(col_clusters, minlength=nclusters_col)
         nel_bnd_clusters = da.bincount(bnd_clusters, minlength=nclusters_bnd)
+        row_cluster_labels = nel_row_clusters.nonzero()[0].compute()
+        col_cluster_labels = nel_col_clusters.nonzero()[0].compute()
+        bnd_cluster_labels = nel_bnd_clusters.nonzero()[0].compute()
         logger.debug(
             'num of populated clusters: row {}, col {}, bnd {}'.format(
-                da.sum(nel_row_clusters > 0).compute(),
-                da.sum(nel_col_clusters > 0).compute(),
-                da.sum(nel_bnd_clusters > 0).compute()
+                len(row_cluster_labels),
+                len(col_cluster_labels),
+                len(bnd_cluster_labels)
             )
         )
-        nel_clusters = da.einsum('i,j->ij', nel_row_clusters, nel_col_clusters)
-        nel_clusters = da.einsum('i,jk->ijk', nel_bnd_clusters, nel_clusters)
+        nel_clusters = da.einsum(
+            'i,j->ij',
+            nel_row_clusters[row_cluster_labels],
+            nel_col_clusters[col_cluster_labels]
+        )
+        nel_clusters = da.einsum(
+            'i,jk->ijk',
+            nel_bnd_clusters[bnd_cluster_labels],
+            nel_clusters
+        )
 
-        R = _setup_cluster_matrix(nclusters_row, row_clusters)
-        C = _setup_cluster_matrix(nclusters_col, col_clusters)
-        B = _setup_cluster_matrix(nclusters_bnd, bnd_clusters)
+        R = _setup_cluster_matrix(row_cluster_labels, row_clusters)
+        C = _setup_cluster_matrix(col_cluster_labels, col_clusters)
+        B = _setup_cluster_matrix(bnd_cluster_labels, bnd_clusters)
 
-        # calculate tri-cluster averages (epsilon takes care of empty clusters)
+        # calculate tri-cluster averages
         # first sum values in each tri-cluster ..
         TriCavg = da.einsum('ij,ilm->jlm', B, Z)  # .. along band axis
         TriCavg = da.einsum('ij,kim->kjm', R, TriCavg)  # .. along row axis
         TriCavg = da.einsum('ij,kli->klj', C, TriCavg)  # .. along col axis
         # finally divide by number of elements in each tri-cluster
-        TriCavg = (TriCavg + Gavg * epsilon) / (nel_clusters + epsilon)
+        TriCavg = TriCavg / nel_clusters
 
         # unpack tri-cluster averages ..
         avg_unpck = da.einsum('ij,jkl->ikl', B, TriCavg)  # .. along band axis
         avg_unpck = da.einsum('ij,klj->kli', C, avg_unpck)  # .. along col axis
         # use these for the row cluster assignment
         idx = (1, 0, 2)
-        d_row = _distance(Z.transpose(idx), avg_unpck.transpose(idx), epsilon)
+        d_row = _distance(Z.transpose(idx), avg_unpck.transpose(idx))
         row_clusters = da.argmin(d_row, axis=1)
 
         # unpack tri-cluster averages ..
@@ -122,14 +129,14 @@ def triclustering(Z, nclusters_row, nclusters_col, nclusters_bnd, errobj,
         avg_unpck = da.einsum('ij,kjl->kil', R, avg_unpck)  # .. along row axis
         # use these for the col cluster assignment
         idx = (2, 0, 1)
-        d_col = _distance(Z.transpose(idx), avg_unpck.transpose(idx), epsilon)
+        d_col = _distance(Z.transpose(idx), avg_unpck.transpose(idx))
         col_clusters = da.argmin(d_col, axis=1)
 
         # unpack tri-cluster averages ..
         avg_unpck = da.einsum('ij,kjl->kil', R, TriCavg)  # .. along row axis
         avg_unpck = da.einsum('ij,klj->kli', C, avg_unpck)  # .. along col axis
         # use these for the band cluster assignment
-        d_bnd = _distance(Z, avg_unpck, epsilon)
+        d_bnd = _distance(Z, avg_unpck)
         bnd_clusters = da.argmin(d_bnd, axis=1)
 
         # Error value (actually just the band component really)
@@ -137,9 +144,12 @@ def triclustering(Z, nclusters_row, nclusters_col, nclusters_bnd, errobj,
         minvals = da.min(d_bnd, axis=1)
         # power 1 divergence, power 2 euclidean
         e = da.sum(da.power(minvals, 1))
-        row_clusters, col_clusters, bnd_clusters, e = client.persist(
-            [row_clusters, col_clusters, bnd_clusters, e]
-        )
+        row_clusters, col_clusters, bnd_clusters, e = client.persist([
+            da.take(row_cluster_labels, row_clusters),
+            da.take(col_cluster_labels, col_clusters),
+            da.take(bnd_cluster_labels, bnd_clusters),
+            e
+        ])
         e = e.compute()
         logger.debug(f'Error = {e:+.15e}, dE = {e - old_e:+.15e}')
         converged = abs(e - old_e) < errobj
