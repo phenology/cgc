c1ac3a73f9d09c8e38a2b7429c52c39d978ecd0d
Francesco Nattino <f.nattino@esciencecenter.nl>
2023-05-05 17:49:53 +0200

    small updates in docs

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'CGC'
-copyright = u'2020, '
-author = u"Netherlands eScience Center"
+project = 'CGC'
+copyright = '2023'
+author = 'Netherlands eScience Center'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -67,7 +67,7 @@ release = cgc.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
